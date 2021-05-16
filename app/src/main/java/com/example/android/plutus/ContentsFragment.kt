b041a661a1827b513f53480cffaf8868e81d1865
Carkzis <marc.jowett@gmail.com>
2021-05-16 18:16:38 +0100

    Attached contents fragment to main activity

---
+++ app/src/main/java/com/example/android/plutus/ContentsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.android.plutus
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.plutus.databinding.FragmentContentsBinding
+
+
+class ContentsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentContentsBinding>(inflater,
+            R.layout.fragment_contents, container, false)
+        return binding.root
+    }
+
+}
