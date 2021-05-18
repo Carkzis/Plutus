692af8349c5cf6735ac09ad9610446a4d87e1ac3
Carkzis <marc.jowett@gmail.com>
2021-05-18 22:03:30 +0100

    Added snackbar and toast extension functions, and refactored lta calc function

--- app/src/main/java/com/example/android/plutus/PclsViewModel.kt
+++ app/src/main/java/com/example/android/plutus/PclsViewModel.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+private const val STANDARD_LTA = 1073100
 
 class PclsViewModel : ViewModel() {
 
@@ -35,6 +39,66 @@ class PclsViewModel : ViewModel() {
     private var _combinedLifetimeAllowance = MutableLiveData<Double>()
     val combinedLifetimeAllowance: LiveData<Double>
         get() = _combinedLifetimeAllowance
+
+    private var pcls: Double = 0.0
+    private var commutedPension: Double = 0.0
+    private var residual: Double = 0.0
+
+    private var pclsLta: Double = 0.0
+    private var residualLta: Double = 0.0
+    private var dcFundLta: Double = 0.0
+
+    private lateinit var lta: String
+
+    private lateinit var decimalFormat: DecimalFormat
+
+    data class Benefits(
+        val pcls: Double,
+        val residualPension: Double,
+        val lta: String,
+        val dcFund: Double)
+
+    fun dbPclsCalculation(
+        fullPension: Double, commutationFactor: Int, dcFund: Double = 0.0): Benefits {
+        // Calculate the pcls and the residual pension
+        pcls = (fullPension * commutationFactor * 20) / ((commutationFactor * 3) + 20)
+        commutedPension = pcls / commutationFactor
+        residual = fullPension - commutedPension
+        lta = ltaCalculation(pcls, residual, dcFund)
+
+        return Benefits(pcls, residual, lta, dcFund)
+    }
+
+    fun cmbPclsCalculation(
+        fullPension: Double, commutationFactor: Int, dcFund: Double = 0.0): Benefits {
+        // Calculate the pcls and residual when combined with the dc fund
+        pcls = dcFund + (commutationFactor * ((fullPension * 20) - (dcFund * 3)) /
+                ((commutationFactor * 3) + 20))
+        commutedPension = (pcls - dcFund) / commutationFactor
+        residual = fullPension - commutedPension
+        lta = ltaCalculation(pcls, residual, dcFund)
+
+        return Benefits(pcls, residual, lta, dcFund)
+    }
+
+    fun ltaCalculation(pcls: Double, residual: Double, dcFund: Double) : String {
+
+        // Variable to format lifetime allowance to 2 decimal places (rounded down).
+        decimalFormat = DecimalFormat("#.##")
+        decimalFormat.roundingMode = RoundingMode.DOWN
+
+        // Calculate the individual LTAs, and convert them back into doubles after rounding.
+        pclsLta =
+            decimalFormat.format((pcls / STANDARD_LTA) * 100).toDouble()
+        residualLta =
+            decimalFormat.format((residual * 20) / STANDARD_LTA * 100).toDouble()
+        dcFundLta =
+            decimalFormat.format((dcFund / STANDARD_LTA) * 100).toDouble()
+
+        // Then add them together to give the total LTA (and convert back to a string).
+        return "${(pclsLta + residualLta + dcFundLta)}%"
+
+    }
 
 }
 
