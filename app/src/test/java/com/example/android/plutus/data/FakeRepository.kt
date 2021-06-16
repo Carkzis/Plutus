2a75600e094ed7a6cf8f0980cd135d16ec63cea3
Carkzis <marc.jowett@gmail.com>
2021-06-16 22:12:59 +0100

    Fake repository created

--- app/src/test/java/com/example/android/plutus/data/FakeRepository.kt
+++ app/src/test/java/com/example/android/plutus/data/FakeRepository.kt
@@ -1,16 +1,35 @@
 package com.example.android.plutus.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.plutus.InflationRate
 import com.example.android.plutus.NetworkInflationRate
+import com.example.android.plutus.util.Benefits
 import org.junit.Assert.*
 import javax.inject.Inject
 
 class FakeRepository @Inject constructor() : Repository {
 
     // This is so that the mapping is tested for.
-    private var cpiNetworkRates: List<NetworkInflationRate> = mutableListOf()
+    private var cpiNetworkRates = MutableLiveData<List<DatabaseCpiInflationRate>>().apply {
+        value = MutableList(5) {
+            DatabaseCpiInflationRate("01/01/1900",
+                "5.0",
+                "N/A",
+                "1900",
+                "January",
+                "Q1",
+                "N/A",
+                "N/A",
+                "1"
+            )
+        }
+    }
 
-    var cpiInflationRates: List<InflationRate> = mutableListOf()
+    var cpiInflationRates: LiveData<List<InflationRate>> = Transformations.map(cpiNetworkRates) {
+        it.asDomainModel()
+    }
 
     private var returnError = true
 
@@ -19,7 +38,28 @@ class FakeRepository @Inject constructor() : Repository {
     }
 
     override suspend fun refreshInflation() {
-        // TODO: Essentially, this will just return a fake list of InflationRates increased by one
-        // TODO: Will map it to from the cpiNetworkRates, like the usual repository.
+        // Test if there is an error.
+        if (returnError) {
+            return
+        }
+
+        // Return a new list of fake values, one size larger to imitate returning updated data.
+        cpiNetworkRates.value = MutableList(6) {
+            DatabaseCpiInflationRate(
+                "01/01/1900",
+                "5.0",
+                "N/A",
+                "1900",
+                "January",
+                "Q1",
+                "N/A",
+                "N/A",
+                "1"
+            )
+        }
+
+        cpiInflationRates = Transformations.map(cpiNetworkRates) {
+            it.asDomainModel()
+        }
     }
 }
