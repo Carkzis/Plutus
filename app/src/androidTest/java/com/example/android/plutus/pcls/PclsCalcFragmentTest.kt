3395b8eb83ef053addbfd5f7554e4b49ecb2a61d
Carkzis <marc.jowett@gmail.com>
2021-06-27 22:47:26 +0100

    Added ui test for pcls fragment for large dc funds

--- app/src/androidTest/java/com/example/android/plutus/pcls/PclsCalcFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/plutus/pcls/PclsCalcFragmentTest.kt
@@ -58,9 +58,9 @@ class PclsCalcFragmentTest {
             .perform(ViewActions.click())
 
         // Confirm we end up with the pcls+residual and full pensions calculations being displayed
-        onView(ViewMatchers.withId(R.id.db_only_linear_layout))
+        onView(ViewMatchers.withId(R.id.opt2_table))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        onView(ViewMatchers.withId(R.id.no_pcls_linear_layout))
+        onView(ViewMatchers.withId(R.id.opt3_table))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
         // Ensure DC funds are not displaying
@@ -69,13 +69,15 @@ class PclsCalcFragmentTest {
         onView(ViewMatchers.withId(R.id.np_dc_result_text))
             .check(ViewAssertions.matches(not(ViewMatchers.isDisplayed())))
 
-        // Confirm the combined pcls linear layout is not there
-        onView(ViewMatchers.withId(R.id.combined_linear_layout))
+        // Confirm the 2 combined pcls result tables are not there
+        onView(ViewMatchers.withId(R.id.opt1_table))
+            .check(ViewAssertions.matches(not(ViewMatchers.isDisplayed())))
+        onView(ViewMatchers.withId(R.id.opt1b_table))
             .check(ViewAssertions.matches(not(ViewMatchers.isDisplayed())))
     }
 
     @Test
-    fun calculationResults_DcFundProvided_displayedInUi() = runBlockingTest {
+    fun calculationResults_smallDcFundProvided_displayedInUi() = runBlockingTest {
         // Start on contents screen, as DataBindingIdlingResource.monitorFragment
         // currently not functioning correctly
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
@@ -99,19 +101,66 @@ class PclsCalcFragmentTest {
             .perform(ViewActions.click())
 
         // Confirm we end up with the pcls calculation results being displayed
-        onView(ViewMatchers.withId(R.id.db_only_linear_layout))
+        onView(ViewMatchers.withId(R.id.opt2_table))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        onView(ViewMatchers.withId(R.id.no_pcls_linear_layout))
+        onView(ViewMatchers.withId(R.id.opt3_table))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
-        // Ensure DC funds are displayed in the DB and full pension linear layouts
+        // Ensure DC funds are displayed in the DB and full pension tables
         onView(ViewMatchers.withId(R.id.db_dc_result_text))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(ViewMatchers.withId(R.id.np_dc_result_text))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
         // Confirm we end up with the combined pcls calculation results being displayed
-        onView(ViewMatchers.withId(R.id.combined_linear_layout))
+        onView(ViewMatchers.withId(R.id.opt1_table))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // Confirm option for taking residual dc as a UFPLS is not displayed
+        onView(ViewMatchers.withId(R.id.opt1b_table))
+            .check(ViewAssertions.matches(not(ViewMatchers.isDisplayed())))
+    }
+
+    @Test
+    fun calculationResults_largeDcFundProvided_displayedInUi() = runBlockingTest {
+        // Start on contents screen, as DataBindingIdlingResource.monitorFragment
+        // currently not functioning correctly
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+        // Click on the pcls calculator button to go to pcls calculator
+        onView(ViewMatchers.withId(R.id.pcls_button))
+            .perform(ViewActions.click())
+
+        // Click the pension edit text, and enter "1500.00"
+        onView(ViewMatchers.withId(R.id.pension_edittext))
+            .perform(typeText("5,000.00"))
+        // Click on the commutation edit text, and enter 15.0
+        onView(ViewMatchers.withId(R.id.commutation_factor_edittext))
+            .perform(typeText("22.0"))
+        // Click on the commutation edit text, and enter 15.0
+        onView(ViewMatchers.withId(R.id.dc_edittext))
+            .perform(typeText("95,000.00"))
+
+        // Click the calculate pcls button
+        onView(ViewMatchers.withId(R.id.calculate_pcls_button))
+            .perform(ViewActions.click())
+
+        // Confirm we end up with the pcls calculation results being displayed
+        onView(ViewMatchers.withId(R.id.opt2_table))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.opt3_table))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // Ensure DC funds are displayed in the DB and full pension tables
+        onView(ViewMatchers.withId(R.id.db_dc_result_text))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.np_dc_result_text))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // Confirm we end up with the both combined pcls calculation results being displayed
+        onView(ViewMatchers.withId(R.id.opt1_table))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.opt1b_table))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
