1ca8e75d8d23757ebd47c272988d47d6e778f2e8
Carkzis <marc.jowett@gmail.com>
2021-09-26 14:16:54 +0100

    Notes added for date classes

--- app/src/main/java/com/carkzis/android/plutus/dates/DateCalcFragment.kt
+++ app/src/main/java/com/carkzis/android/plutus/dates/DateCalcFragment.kt
@@ -16,6 +16,10 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
+/**
+ * Fragment for allowing the user to calculate the duration between two dates,
+ * which will then show on the UI the duration in different units of time e.g. years, months.
+ */
 @AndroidEntryPoint
 class DateCalcFragment : Fragment() {
 
@@ -28,6 +32,11 @@ class DateCalcFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        /*
+         Set up data binding between the fragment and the layout. The lifecycleOwner observes
+         the changes in LiveData in this databinding.
+         */
         viewDataBinding = FragmentDateCalcBinding.inflate(inflater, container, false).apply {
             dateCalcViewModel = viewModel
             lifecycleOwner = viewLifecycleOwner
@@ -37,6 +46,9 @@ class DateCalcFragment : Fragment() {
 
     }
 
+    /**
+     * Used here to set up various observers/listeners.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -48,20 +60,32 @@ class DateCalcFragment : Fragment() {
 
     }
 
+    /**
+     * This sets up opening the DatePickerDialog when the edittext view for the start date
+     * is clicked on.  This restricts the user to only providing valid dates.
+     */
     private fun setUpStartDateDialog() {
         viewDataBinding.startDateEdittext.setOnClickListener {
+            // Set up the initial date shown in the dialog.
             val calendar = setUpCalendar(viewModel.startDateInfo.value!!)
             DatePickerDialog(requireContext(), {
                     _, y, m, d ->
-                // need to add 1 to month as it is indexed at 0 for some bizarre reason
-                // (as years and days are not?)
+                /*
+                 Need to add 1 to month as it is indexed at 0 for some bizarre reason
+                 (would be fine but years and days are not?)
+                 */
                     viewModel.setStartDate(y, m + 1, d)
             }, calendar.year, calendar.month, calendar.day).show()
         }
     }
 
+    /**
+     * This sets up opening the DatePickerDialog when the edittext view for the end date
+     * is clicked on.  This restricts the user to only providing valid dates.
+     */
     private fun setUpEndDateDialog() {
         viewDataBinding.endDateEdittext.setOnClickListener {
+            // Set up the initial date shown in the dialog.
             val calendar = setUpCalendar(viewModel.endDateInfo.value!!)
             DatePickerDialog(requireContext(), {
                     _, y, m, d ->
@@ -70,6 +94,12 @@ class DateCalcFragment : Fragment() {
         }
     }
 
+    /**
+     * This sets up the initial date shown when opening the calendar.  It will either be the
+     * current date if no start or end date has been previously selected, or the previous start
+     * or end date selected (for example, if the user opens the calendar again after having
+     * previously selected a date).
+     */
     private fun setUpCalendar(startOrEndDate: String): CalendarInfo {
         return if (startOrEndDate == "") {
             val calendar: Calendar = Calendar.getInstance()
@@ -88,18 +118,27 @@ class DateCalcFragment : Fragment() {
         }
     }
 
+    /**
+     * Sets a click listener to the calculation button.
+     */
     private fun setUpButton() {
         viewDataBinding.dateCalcButton.setOnClickListener {
             viewModel.validateBeforeCalculation()
         }
     }
 
+    /**
+     * Sets up the default date calculation results to 0 across the board.
+     */
     private fun addDefaultResults() {
         defaultResults = DateCalcResults(
             0, 0, 0, 0, Pair(0, 0), Pair(0, 0), 0, 0)
         viewModel.addDefaultResultsVM(defaultResults)
     }
 
+    /**
+     * Sets up the ability to show a toast once by observing the LiveData in the ViewModel.
+     */
     private fun setUpToast() {
         viewModel.toastText.observe(viewLifecycleOwner, {
             it.getContextIfNotHandled()?.let { message ->
--- app/src/main/java/com/carkzis/android/plutus/dates/DateCalcViewModel.kt
+++ app/src/main/java/com/carkzis/android/plutus/dates/DateCalcViewModel.kt
@@ -17,34 +17,55 @@ import com.carkzis.android.plutus.util.yearsCalculation
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+/**
+ * ViewModel for the DateCalcFragment.
+ */
 @HiltViewModel
 class DateCalcViewModel @Inject constructor() : ViewModel() {
 
     var startDateInfo = MutableLiveData("")
     var endDateInfo = MutableLiveData("")
 
+    /**
+     * This holds the calculation results.
+     */
     private var _dateCalcResults = MutableLiveData<DateCalcResults>()
     val dateCalcResults: LiveData<DateCalcResults>
         get() = _dateCalcResults
 
+    /**
+     * This LiveData holds a toast event, taking in an Int which will be a String id.
+     */
     private var _toastText = MutableLiveData<Event<Int>>()
     val toastText: LiveData<Event<Int>>
         get() = _toastText
 
     private lateinit var results: DateCalcResults
 
+    /**
+     * This sets the start date edittext with the data provided in the correct format.
+     */
     fun setStartDate(year: Int, month: Int, day: Int) {
+        // Ensure single digits have a preceding 0.
         val formattedMonth = if (month < 10) "0$month" else month
         val formattedDay = if (day < 10) "0$day" else day
         startDateInfo.value = "$formattedDay/$formattedMonth/$year"
     }
 
+    /**
+     * This sets the end date edittext with the data provided in the correct format.
+     */
     fun setEndDate(year: Int, month: Int, day: Int) {
+        // Ensure single digits have a preceding 0.
         val formattedMonth = if (month < 10) "0$month" else month
         val formattedDay = if (day < 10) "0$day" else day
         endDateInfo.value = "$formattedDay/$formattedMonth/$year"
     }
 
+    /**
+     * Validates the entered data, and returns an error message if there is something missing
+     * or incorrect.
+     */
     fun validateBeforeCalculation() {
         // Return if these values have not been entered.
         if (startDateInfo.value == "") return showToastMessage(R.string.no_start_date_entered)
@@ -52,14 +73,16 @@ class DateCalcViewModel @Inject constructor() : ViewModel() {
 
         val periodDays = daysCalculation(startDateInfo.value!!, endDateInfo.value!!)
 
-        // If the difference is negative, return a toast message
+        // If the difference is negative, return a toast message.
         if (periodDays < 1) return showToastMessage(R.string.end_date_after_start_date)
 
         calculateDateDifferences()
     }
 
+    /**
+     * Returns a result data class, using the various calculations held in CalcUtils.kt.
+     */
     private fun calculateDateDifferences() {
-
         results = DateCalcResults(
             yearsCalculation(startDateInfo.value!!, endDateInfo.value!!),
             monthsCalculation(startDateInfo.value!!, endDateInfo.value!!),
@@ -73,11 +96,17 @@ class DateCalcViewModel @Inject constructor() : ViewModel() {
         _dateCalcResults.value = results
     }
 
+    /**
+     * Assigns the defaults (0 across the board) if there is no calculation completed.
+     */
     fun addDefaultResultsVM(defaults: DateCalcResults) {
         // Will only assign the defaults if the value is null.
         _dateCalcResults.value = _dateCalcResults.value ?: defaults
     }
 
+    /**
+     * Add the toast string id, in an Event class, to the LiveData.
+     */
     private fun showToastMessage(message: Int) {
         _toastText.value = Event(message)
     }
